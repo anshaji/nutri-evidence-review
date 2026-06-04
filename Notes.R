bb8ca3b7c4e37d5cf4b9ffdb2e94c83094b59bf0
Akash Shaji <anshaji@users.noreply.github.com>
2026-06-03 17:17:21 -0700

    notes

---
+++ Notes.R
@@ -0,0 +1,19 @@
+# Preventative vs Treatment 
+# Outcome and target population
+
+
+#Caveat Slide - 
+
+# Slide - How we scoped it? Context
+
+# What makes it credible - slop vs dependable 
+
+# PICO - check out web of science alternative 
+# Two phase model - first phase Interventions and Effect Sizes 
+# Phase two - cost effectiveness 
+
+
+
+# Cost effectivness on X - Effect Size Y
+
+# Automated - remove - efficient rapid evidence review process
